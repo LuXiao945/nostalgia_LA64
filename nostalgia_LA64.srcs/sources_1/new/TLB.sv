dec5f0d7392cf2d40fcff5b84223a4af200ea7de
Tur1ngCh1ck3n <13328035396@163.com>
2026-09-18 17:32:19 +0800

    tlb索引寻址

--- nostalgia_LA64.srcs/sources_1/new/TLB.sv
+++ nostalgia_LA64.srcs/sources_1/new/TLB.sv
@@ -21,25 +21,37 @@ module TLB(
 	output reg nx_i,//不可执行位
 	output reg nr_i,//不可读位
 	output reg rplv_i,//受限特权等级使能（RPLV），1比特。页表项是否仅被对应特权等级的程序访问的控制位。请参
-	//数据端口--------------------------------------------------------
+	//数据读端口--------------------------------------------------------
+	input logic [7:0] TLBWR_index,//TLBWR指令输入的索引，此指令从d端访问
 	input address_mode,//寻址模式 0为vaddr寻址 1为tlb索引寻址
 	input en_d,//使能信号
     input rw_d, //读写控制信号0=r 1=w
 	input [63:0] vaddr_d,
 	input [9:0] asid_d,
 	output reg hit_d,
-	output reg [47:0] ppn_d,
 	output reg [5:0] ps_d,
-	output reg [1:0] plv_d,
-	output reg [1:0] mat_d,
-	output reg d_d,
-	output reg v_d,
-	output reg nx_d,
-	output reg nr_d,
-	output reg rplv_d,
 	
+	output reg [47:0] ppn_d0,
+	output reg [1:0] plv_d0,
+	output reg [1:0] mat_d0,
+	output reg d_d0,
+	output reg v_d0,
+	output reg nx_d0,
+	output reg nr_d0,
+	output reg rplv_d0,
+
+	output reg [47:0] ppn_d1,
+	output reg [1:0] plv_d1,
+	output reg [1:0] mat_d1,
+	output reg d_d1,
+	output reg v_d1,
+	output reg nx_d1,
+	output reg nr_d1,
+	output reg rplv_d1,
+	
+	output logic odd_even_sel_d,//tlb奇偶页选择，1为奇数页 0为偶数页
 	output logic [7:0] TLB_index//TLBSRCH指令输出的索引，此指令从d端访问
-	
+	//数据写端口
     );
 
     //记录命中的页表项，将页表项的数据输出
@@ -70,7 +82,7 @@ module TLB(
   				.douta(stlb_c_i[i]),  // output wire [52 : 0] douta
   				.clkb(CLK),    // input wire clkb
   				.web(),      // input wire [0 : 0] web
-  				.addrb(vaddr_d[16:13]),  // input wire [3 : 0] addrb
+  				.addrb((address_mode ? TLBWR_index[6:3] : vaddr_d[16:13])),  // input wire [3 : 0] addrb
   				.dinb(),    // input wire [52 : 0] dinb
   				.doutb(stlb_c_d[i])  // output wire [52 : 0] doutb
 				);	
@@ -92,7 +104,7 @@ module TLB(
  			 .douta(stlb_d0_i[j]),  // output wire [44 : 0] douta
   			.clkb(CLK),    // input wire clkb
   			.web(),      // input wire [0 : 0] web
-  			.addrb(vaddr_d[16:13]),  // input wire [3 : 0] addrb
+  			.addrb((address_mode ? TLBWR_index[6:3] : vaddr_d[16:13])),  // input wire [3 : 0] addrb
   			.dinb(),    // input wire [44 : 0] dinb
   			.doutb(stlb_d0_d[j])  // output wire [44 : 0] doutb
 			);
@@ -112,7 +124,7 @@ module TLB(
  			 .douta(stlb_d1_i[k]),  // output wire [44 : 0] douta
   			.clkb(CLK),    // input wire clkb
   			.web(),      // input wire [0 : 0] web
-  			.addrb(vaddr_d[16:13]),  // input wire [3 : 0] addrb
+  			.addrb((address_mode ? TLBWR_index[6:3] : vaddr_d[16:13])),  // input wire [3 : 0] addrb
   			.dinb(),    // input wire [44 : 0] dinb
   			.doutb(stlb_d1_d[k])  // output wire [44 : 0] doutb
 			);
@@ -121,9 +133,10 @@ module TLB(
 	
 	//判断L1 TLB是否命中-------------------------------------------------------------
 	reg odd_even_sel_i;//tlb奇偶页选择，1为奇数页 0为偶数页 
-    reg odd_even_sel_d;
+    //reg odd_even_sel_d;定义在输出端口
     reg [47:0] temp_ppn_i;//临时储存ppn
-    reg [47:0] temp_ppn_d;
+    logic [47:0] temp_ppn_d0;
+	logic [47:0] temp_ppn_d1;
     reg [5:0] temp_ps_i;//零时存储ps
     reg [5:0] temp_ps_d;
 	//判断i端
@@ -156,20 +169,32 @@ module TLB(
 		d_hit_way = 5'b0;
     	d_hit_op = 1'b0;
     	//判断stlb d端是否命中
-		for(b=0;b<8;b=b+1)begin
-			if((vaddr_d[47:17]==stlb_c_d[b][46:16])//判断va的tage位与tlb中保存的是否相等
-			&&stlb_c_d[b][0]//tlb中0位是有效位
-			&&((asid_d==stlb_c_d[b][9:1])//判断asid和tlb中是否相等
-			||stlb_c_d[b][11]))//asid相等或者全局标志位tlb[11]其中任意一个为1时tlb命中
-			begin
-				hit_d = 1'b1;
-				d_hit_way = b;
-    			d_hit_op = 1'b0;
-    			
-    			odd_even_sel_d = vaddr_d[12];
-    			break;
+		if (address_mode) begin//寻址模式 0为vaddr寻址 1为tlb索引寻址
+			//索引寻址
+			if (TLBWR_index[7]) begin//TLBWR_index[7]为1表示在mtlb中,0表示在stlb中
+				hit_d = stlb_c_d[TLBWR_index[2:0]][0];//TLBWR_index[2:0]指向每路中的way
+				d_hit_op = 1'b0;
+			end else begin
+				hit_d = mtlb_c[TLBWR_index[4:0]][0];
+				d_hit_op = 1'b1;//0说明在stlb中命中，1说明在mtlb中命中
 			end
 			
+
+		end else begin
+			for(b=0;b<8;b=b+1)begin
+				if((vaddr_d[47:17]==stlb_c_d[b][46:16])//判断va的tage位与tlb中保存的是否相等
+				&&stlb_c_d[b][0]//tlb中0位是有效位
+				&&((asid_d==stlb_c_d[b][9:1])//判断asid和tlb中是否相等
+				||stlb_c_d[b][11]))//asid相等或者全局标志位tlb[11]其中任意一个为1时tlb命中
+				begin
+					hit_d = 1'b1;
+					d_hit_way = b;
+    				d_hit_op = 1'b0;
+	
+    				odd_even_sel_d = vaddr_d[12];
+    				break;
+				end
+			end
 		end
 		//判断mtlb是否命中
 		for(e=0;e<32;e=e+1)begin
@@ -231,8 +256,9 @@ module TLB(
     					temp_ps_i = mtlb_c[e][17:12];
     				end
     			end
-    			//判断d端
-    			if(mtlb_c[e][52:18]==vaddr_d[47:13])begin
+    			//判断d端                              
+				//当addr寻址模式时判断是否命中,索引寻址单独判断
+    			if((mtlb_c[e][52:18]==vaddr_d[47:13])&&(!address_mode))begin
     				//判断asid和全局位是否有一个有效
     				if((asid_d==mtlb_c[e][9:1])||(mtlb_c[e][11]))begin
     					hit_d = 1'b1;
@@ -242,8 +268,9 @@ module TLB(
     					odd_even_sel_d = vaddr_d[mtlb_c[e][17:12]];
     					//计算ppn
     					case(odd_even_sel_d)
-    						1'b0:temp_ppn_d = (mtlb_d0[e][44:9]&address_mask)<<12|(~(address_mask<<12)&vaddr_d[47:0]);
-    						1'b1:temp_ppn_d = (mtlb_d1[e][44:9]&address_mask)<<12|(~(address_mask<<12)&vaddr_d[47:0]);
+    						1'b0:temp_ppn_d0 = (mtlb_d0[e][44:9]&address_mask)<<12|(~(address_mask<<12)&vaddr_d[47:0]);
+    						1'b1:temp_ppn_d1 = (mtlb_d1[e][44:9]&address_mask)<<12|(~(address_mask<<12)&vaddr_d[47:0]);
+							default:;
     					endcase
     					//储存ps
     					temp_ps_d = mtlb_c[e][17:12];
@@ -252,12 +279,14 @@ module TLB(
     		end
     		
     	end
+		
 	end
 
 	    //读写逻辑
 
 //    assign odd_even_sel_i = vaddr_i[12];//虚拟地址设计为48位 第13位为奇偶页表判断位 指令集手册92页
  //   assign odd_even_sel_d = vaddr_d[12];
+	
     always@(*)begin
     	//读出
     	//指令
@@ -318,65 +347,62 @@ module TLB(
 			endcase	
     	end
     	//数据-读
+		/*
+		d端为了支持tlb维护指令,一次读取把奇偶双页全部取出
+		并且读写data端口复用
+		*/
     	if(hit_d&&!rw_d&&en_d)begin
 			case(d_hit_op)
 				1'b0:begin//0说明在stlb中命中，1说明在mtlb中命中
-						TLB_index = {1'b0,vaddr_d[16:13],d_hit_way[2:0]};
-						case(odd_even_sel_d)
-							1'b0:begin
-									ppn_d  = {stlb_d0_d[d_hit_way[2:0]][44:9],vaddr_d[11:0]};
-									ps_d   = 6'b001100;//stlb中页大小统一为4kb
-									plv_d  = stlb_d0_d[d_hit_way[2:0]][7:6];
-									mat_d  = stlb_d0_d[d_hit_way[2:0]][5:4];
-									d_d    = stlb_d0_d[d_hit_way[2:0]][1];
-									v_d    = stlb_d0_d[d_hit_way[2:0]][0];
-									nx_d   = stlb_d0_d[d_hit_way[2:0]][3];
-									nr_d   = stlb_d0_d[d_hit_way[2:0]][2];
-									rplv_d = stlb_d0_d[d_hit_way[2:0]][8];
-								end
-							1'b1:begin
-									ppn_d  = {stlb_d1_d[d_hit_way[2:0]][44:9],vaddr_d[11:0]};
-									ps_d   = 6'b001100;//stlb中页大小统一为4kb
-									plv_d  = stlb_d1_d[d_hit_way[2:0]][7:6];
-									mat_d  = stlb_d1_d[d_hit_way[2:0]][5:4];
-									d_d    = stlb_d1_d[d_hit_way[2:0]][1];
-									v_d    = stlb_d1_d[d_hit_way[2:0]][0];
-									nx_d   = stlb_d1_d[d_hit_way[2:0]][3];
-									nr_d   = stlb_d1_d[d_hit_way[2:0]][2];
-									rplv_d = stlb_d1_d[d_hit_way[2:0]][8];
-								end 
-						endcase
+						ps_d   = 6'b001100;//stlb中页大小统一为4kb
+
+						ppn_d0  = {stlb_d0_d[d_hit_way[2:0]][44:9],vaddr_d[11:0]};
+						plv_d0  = stlb_d0_d[d_hit_way[2:0]][7:6];
+						mat_d0  = stlb_d0_d[d_hit_way[2:0]][5:4];
+						d_d0    = stlb_d0_d[d_hit_way[2:0]][1];
+						v_d0    = stlb_d0_d[d_hit_way[2:0]][0];
+						nx_d0   = stlb_d0_d[d_hit_way[2:0]][3];
+						nr_d0   = stlb_d0_d[d_hit_way[2:0]][2];
+						rplv_d0 = stlb_d0_d[d_hit_way[2:0]][8];
+						//--------------------------------------------
+						ppn_d1  = {stlb_d1_d[d_hit_way[2:0]][44:9],vaddr_d[11:0]};
+						plv_d1  = stlb_d1_d[d_hit_way[2:0]][7:6];
+						mat_d1  = stlb_d1_d[d_hit_way[2:0]][5:4];
+						d_d1    = stlb_d1_d[d_hit_way[2:0]][1];
+						v_d1    = stlb_d1_d[d_hit_way[2:0]][0];
+						nx_d1   = stlb_d1_d[d_hit_way[2:0]][3];
+						nr_d1   = stlb_d1_d[d_hit_way[2:0]][2];
+						rplv_d1 = stlb_d1_d[d_hit_way[2:0]][8];
+					
 					end
 				1'b1:begin
-						TLB_index = 8'b01111111 + 8'(d_hit_way) + 1'b1;
-						case(odd_even_sel_d)//指令奇偶页选择，1为奇数页 0为偶数页 
-							1'b0:begin
-									ppn_d  = temp_ppn_d;
-									ps_d   = temp_ps_d;
-									plv_d  = mtlb_d0[d_hit_way][7:6];
-									mat_d  = mtlb_d0[d_hit_way][5:4];
-									d_d    = mtlb_d0[d_hit_way][1];
-									v_d    = mtlb_d0[d_hit_way][0];
-									nx_d   = mtlb_d0[d_hit_way][3];
-									nr_d   = mtlb_d0[d_hit_way][2];
-									rplv_d = mtlb_d0[d_hit_way][8];
-								end          
-							1'b1:begin       
-									ppn_d  = temp_ppn_d;
-									ps_d   = temp_ps_d;
-									plv_d  = mtlb_d1[d_hit_way][7:6];
-									mat_d  = mtlb_d1[d_hit_way][5:4];
-									d_d    = mtlb_d1[d_hit_way][1];
-									v_d    = mtlb_d1[d_hit_way][0];
-									nx_d   = mtlb_d1[d_hit_way][3];
-									nr_d   = mtlb_d1[d_hit_way][2];
-									rplv_d = mtlb_d1[d_hit_way][8];
-								end 
-						endcase
+						ps_d   = temp_ps_d;
+
+						ppn_d0  = temp_ppn_d0;
+						plv_d0  = mtlb_d0[d_hit_way][7:6];
+						mat_d0  = mtlb_d0[d_hit_way][5:4];
+						d_d0    = mtlb_d0[d_hit_way][1];
+						v_d0    = mtlb_d0[d_hit_way][0];
+						nx_d0   = mtlb_d0[d_hit_way][3];
+						nr_d0   = mtlb_d0[d_hit_way][2];
+						rplv_d0 = mtlb_d0[d_hit_way][8];
+						//---------------------------------------------
+						ppn_d1  = temp_ppn_d1;
+						plv_d1  = mtlb_d1[d_hit_way][7:6];
+						mat_d1  = mtlb_d1[d_hit_way][5:4];
+						d_d1    = mtlb_d1[d_hit_way][1];
+						v_d1    = mtlb_d1[d_hit_way][0];
+						nx_d1   = mtlb_d1[d_hit_way][3];
+						nr_d1   = mtlb_d1[d_hit_way][2];
+						rplv_d1 = mtlb_d1[d_hit_way][8];
+						
 					end 
+				default:;
 			endcase	
     	end
     end
     
-	
+	//0说明在stlb中命中，1说明在mtlb中命中   索引号先从stlb分配再mtlb,mtlb从stlb(128项8'b01111111)的基础上加
+	assign TLB_index = d_hit_op ? (8'b01111111 + 8'(d_hit_way) + 1'b1) : ({1'b0,vaddr_d[16:13],d_hit_way[2:0]});
+
 endmodule
