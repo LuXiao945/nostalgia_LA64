76511b93d0b08503fc2690f37c1718ebbf3eb935
Tur1ngCh1ck3n <13328035396@163.com>
2026-09-26 14:58:10 +0800

    增加了tlb替换算法(PLRU)

--- nostalgia_LA64.srcs/sources_1/new/TLB.sv
+++ nostalgia_LA64.srcs/sources_1/new/TLB.sv
@@ -8,6 +8,7 @@
 module TLB(
 
     input CLK,
+	input RST,//用来复位伪LRU和mtlb写指针
     //指令端口-----------------------------------------------------
     input en_i,//使能信号
     input rw_i, //读写控制信号0=r 1=w
@@ -86,7 +87,10 @@ module TLB(
 	当被填入的页表项的
 	页大小与STLB所配置的页大小（CSR.STLBPS）相等时将被填入STLB，否则将被填入MTLB。
 	*/
-	input logic w_mode//0为索引写,1为根据lru写
+	input logic w_mode,//0为索引写,1为根据lru写
+
+	//0为填0,1为根据CSR.TLBRERA.IsTLBR和CSR.TLBIDX.NE填入有效位
+	input logic exists_bit_choose//写数据(不管是写和无效化)时选择填入比较表项中E位的值
     );
 	/*
 	temp_TLB_index用来存储实际访问tlb表项的索引
@@ -95,7 +99,11 @@ module TLB(
 	*/
 	logic [7:0] indexcnt;
 	logic [7:0] temp_TLB_index;
-    //记录命中的页表项，将页表项的数据输出
+    
+	//读tlb d端的地址
+	logic [3:0] dtlb_addr;
+	assign dtlb_addr = address_mode ? TLBWR_index[6:3] : vaddr_d[16:13];
+	//记录命中的页表项，将页表项的数据输出
     reg [4:0] i_hit_way;
     reg i_hit_op;//0说明在stlb中命中，1说明在mtlb中命中
     reg [4:0] d_hit_way;
@@ -107,11 +115,15 @@ module TLB(
     reg [44:0] mtlb_d0 [31:0];
     reg [44:0] mtlb_d1 [31:0];
     reg [35:0] address_mask;
+	//mtlb_LRU
+	logic [30:0] LRU_M;
     //stlb比较部分---------------------------------------------------------------------------------
     //实例化8个sram存储stlb比较部分
     //a为指令tlb端口    b为数据tlb端口
     wire [46:0] stlb_c_i [7:0];//接收指令端每个stlb比较部分的数据
     wire [46:0] stlb_c_d [7:0];//接收数据端每个stlb比较部分的数据
+	//stlb的lru算法
+	logic [6:0] LRU_S [15:0];
     genvar i;
     generate 
 		for(i=0;i<8;i=i+1)begin : STLB_C
@@ -123,7 +135,7 @@ module TLB(
   				.douta(stlb_c_i[i]),  // output wire [52 : 0] douta
   				.clkb(CLK),    // input wire clkb
   				.web(),      // input wire [0 : 0] web
-  				.addrb((address_mode ? TLBWR_index[6:3] : vaddr_d[16:13])),  // input wire [3 : 0] addrb
+  				.addrb(dtlb_addr),  // input wire [3 : 0] addrb
   				.dinb(),    // input wire [52 : 0] dinb
   				.doutb(stlb_c_d[i])  // output wire [52 : 0] doutb
 				);	
@@ -145,7 +157,7 @@ module TLB(
  			 .douta(stlb_d0_i[j]),  // output wire [44 : 0] douta
   			.clkb(CLK),    // input wire clkb
   			.web(),      // input wire [0 : 0] web
-  			.addrb((address_mode ? TLBWR_index[6:3] : vaddr_d[16:13])),  // input wire [3 : 0] addrb
+  			.addrb(dtlb_addr),  // input wire [3 : 0] addrb
   			.dinb(),    // input wire [44 : 0] dinb
   			.doutb(stlb_d0_d[j])  // output wire [44 : 0] doutb
 			);
@@ -165,7 +177,7 @@ module TLB(
  			 .douta(stlb_d1_i[k]),  // output wire [44 : 0] douta
   			.clkb(CLK),    // input wire clkb
   			.web(),      // input wire [0 : 0] web
-  			.addrb((address_mode ? TLBWR_index[6:3] : vaddr_d[16:13])),  // input wire [3 : 0] addrb
+  			.addrb(dtlb_addr),  // input wire [3 : 0] addrb
   			.dinb(),    // input wire [44 : 0] dinb
   			.doutb(stlb_d1_d[k])  // output wire [44 : 0] doutb
 			);
@@ -182,7 +194,7 @@ module TLB(
     reg [5:0] temp_ps_d;
 	//判断i端
 	integer e;//用于遍历mtlb，判断mtlb是否命中
-	integer a;
+	//integer a;
 	integer b;
 	always@(*)begin
 		//初始化参数
@@ -190,14 +202,14 @@ module TLB(
 		i_hit_way = 5'b0;
     	i_hit_op = 1'b0;
     	//判断stlb i端是否命中
-		for(a=0;a<8;a=a+1)begin
+		for(int unsigned a=0;a<8;a=a+1)begin
 			if((vaddr_i[47:17]==stlb_c_i[a][46:16])//判断va的tage位与tlb中保存的是否相等
 			&&stlb_c_i[a][0]//tlb中0位是有效位
 			&&((asid_i==stlb_c_i[a][9:1])//判断asid和tlb中是否相等
 			||stlb_c_i[a][11]))//asid相等或者全局标志位tlb[11]其中任意一个为1时tlb命中
 			begin
 				hit_i = 1'b1;
-				i_hit_way = a;
+				i_hit_way = 5'(a);
     			i_hit_op = 1'b0;
     			
     			odd_even_sel_i = vaddr_i[12];
@@ -212,12 +224,15 @@ module TLB(
     	//判断stlb d端是否命中
 		if (address_mode) begin//寻址模式 0为vaddr寻址 1为tlb索引寻址
 			//索引寻址
+
 			if (TLBWR_index[7]) begin//TLBWR_index[7]为1表示在mtlb中,0表示在stlb中
 				hit_d = stlb_c_d[TLBWR_index[2:0]][0];//TLBWR_index[2:0]指向每路中的way
 				d_hit_op = 1'b0;
+				d_hit_way = TLBWR_index[4:0];
 			end else begin
 				hit_d = mtlb_c[TLBWR_index[4:0]][0];
 				d_hit_op = 1'b1;//0说明在stlb中命中，1说明在mtlb中命中
+				d_hit_way = {2'b0,TLBWR_index[2:0]};
 			end
 			
 
@@ -446,5 +461,189 @@ module TLB(
 	//0说明在stlb中命中，1说明在mtlb中命中   索引号先从stlb分配再mtlb,mtlb从stlb(128项8'b01111111)的基础上加
 	assign TLB_index = d_hit_op ? (8'b01111111 + 8'(d_hit_way) + 1'b1) : ({1'b0,vaddr_d[16:13],d_hit_way[2:0]});
 	//写入逻辑
+	logic TLB_ebit;//写入tlb的存在位
+	assign TLB_ebit = exists_bit_choose ? (CSR_TLBRERA_IsTLBR||(!CSR_TLBIDX_NE)) : 1'b0 ;
 
+	always_ff@(posedge CLK)begin
+		if(RST)begin//复位mtlb写指针和stlb伪LRU
+			LRU_M <= 31'b0;
+			for (int unsigned a=0;a<16;a++) begin
+				LRU_S[a] <= 7'b0; 
+			end
+		end else begin
+			if (w_mode) begin//0为索引写,1为根据lru写
+				
+			end else begin
+				
+			end
+			//读取时更新LRU
+			/*
+			i端和d端分别命中stlb和mtlb不会产生冲突
+			如果都在mtlb命中那么产生冲突，如果都命中stlb那么如果命中同一组则产生冲突
+			(vaddr_i[16:13]==vaddr_d[16:13])&&(hit_i&&hit_d)&&(i_hit_op==d_hit_op)
+			*/
+			if ((hit_d && hit_i)&&(i_hit_op==d_hit_op)&&((i_hit_op==1'b1)||(vaddr_i[16:13]==dtlb_addr))) begin//i和d都命中并且命中了同一路,此时根据i端来更新
+				if (i_hit_op) begin//mtlb  0说明在stlb中命中，1说明在mtlb中命中
+					case(i_hit_way)
+						5'd0 :begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b1; LRU_M[3] <=1'b1; LRU_M[7] <=1'b1; LRU_M[15]<=1'b1;end
+						5'd1 :begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b1; LRU_M[3] <=1'b1; LRU_M[7] <=1'b1; LRU_M[15]<=1'b0;end
+						5'd2 :begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b1; LRU_M[3] <=1'b1; LRU_M[7] <=1'b0; LRU_M[16]<=1'b1;end
+						5'd3 :begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b1; LRU_M[3] <=1'b1; LRU_M[7] <=1'b0; LRU_M[16]<=1'b0;end
+						5'd4 :begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b1; LRU_M[3] <=1'b0; LRU_M[8] <=1'b1; LRU_M[17]<=1'b1;end
+						5'd5 :begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b1; LRU_M[3] <=1'b0; LRU_M[8] <=1'b1; LRU_M[17]<=1'b0;end
+						5'd6 :begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b1; LRU_M[3] <=1'b0; LRU_M[8] <=1'b0; LRU_M[18]<=1'b1;end
+						5'd7 :begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b1; LRU_M[3] <=1'b0; LRU_M[8] <=1'b0; LRU_M[18]<=1'b0;end
+						5'd8 :begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b0; LRU_M[4] <=1'b1; LRU_M[9] <=1'b1; LRU_M[19]<=1'b1;end
+						5'd9 :begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b0; LRU_M[4] <=1'b1; LRU_M[9] <=1'b1; LRU_M[19]<=1'b0;end
+						5'd10:begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b0; LRU_M[4] <=1'b1; LRU_M[9] <=1'b0; LRU_M[20]<=1'b1;end
+						5'd11:begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b0; LRU_M[4] <=1'b1; LRU_M[9] <=1'b0; LRU_M[20]<=1'b0;end
+						5'd12:begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b0; LRU_M[4] <=1'b0; LRU_M[10]<=1'b1; LRU_M[21]<=1'b1;end
+						5'd13:begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b0; LRU_M[4] <=1'b0; LRU_M[10]<=1'b1; LRU_M[21]<=1'b0;end
+						5'd14:begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b0; LRU_M[4] <=1'b0; LRU_M[10]<=1'b0; LRU_M[22]<=1'b1;end
+						5'd15:begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b0; LRU_M[4] <=1'b0; LRU_M[10]<=1'b0; LRU_M[22]<=1'b0;end
+						5'd16:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b1; LRU_M[5] <=1'b1; LRU_M[11]<=1'b1; LRU_M[23]<=1'b1;end
+						5'd17:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b1; LRU_M[5] <=1'b1; LRU_M[11]<=1'b1; LRU_M[23]<=1'b0;end
+						5'd18:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b1; LRU_M[5] <=1'b1; LRU_M[11]<=1'b0; LRU_M[24]<=1'b1;end
+						5'd19:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b1; LRU_M[5] <=1'b1; LRU_M[11]<=1'b0; LRU_M[24]<=1'b0;end
+						5'd20:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b1; LRU_M[5] <=1'b0; LRU_M[12]<=1'b1; LRU_M[25]<=1'b1;end
+						5'd21:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b1; LRU_M[5] <=1'b0; LRU_M[12]<=1'b1; LRU_M[25]<=1'b0;end
+						5'd22:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b1; LRU_M[5] <=1'b0; LRU_M[12]<=1'b0; LRU_M[26]<=1'b1;end
+						5'd23:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b1; LRU_M[5] <=1'b0; LRU_M[12]<=1'b0; LRU_M[26]<=1'b0;end
+						5'd24:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b0; LRU_M[6] <=1'b1; LRU_M[13]<=1'b1; LRU_M[27]<=1'b1;end
+						5'd25:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b0; LRU_M[6] <=1'b1; LRU_M[13]<=1'b1; LRU_M[27]<=1'b0;end
+						5'd26:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b0; LRU_M[6] <=1'b1; LRU_M[13]<=1'b0; LRU_M[28]<=1'b1;end
+						5'd27:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b0; LRU_M[6] <=1'b1; LRU_M[13]<=1'b0; LRU_M[28]<=1'b0;end
+						5'd28:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b0; LRU_M[6] <=1'b0; LRU_M[14]<=1'b1; LRU_M[29]<=1'b1;end
+						5'd29:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b0; LRU_M[6] <=1'b0; LRU_M[14]<=1'b1; LRU_M[29]<=1'b0;end
+						5'd30:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b0; LRU_M[6] <=1'b0; LRU_M[14]<=1'b0; LRU_M[30]<=1'b1;end
+						5'd31:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b0; LRU_M[6] <=1'b0; LRU_M[14]<=1'b0; LRU_M[30]<=1'b0;end
+						default:;
+  					endcase
+				end else begin//stlb
+					case(i_hit_way[2:0])
+  						3'b000:begin LRU_S[vaddr_i[16:13]][0] <= 1;LRU_S[vaddr_i[16:13]][1] <= 1;LRU_S[vaddr_i[16:13]][3] <= 1;end
+  						3'b001:begin LRU_S[vaddr_i[16:13]][0] <= 1;LRU_S[vaddr_i[16:13]][1] <= 1;LRU_S[vaddr_i[16:13]][3] <= 0;end
+  						3'b010:begin LRU_S[vaddr_i[16:13]][0] <= 1;LRU_S[vaddr_i[16:13]][1] <= 0;LRU_S[vaddr_i[16:13]][4] <= 1;end
+  						3'b011:begin LRU_S[vaddr_i[16:13]][0] <= 1;LRU_S[vaddr_i[16:13]][1] <= 0;LRU_S[vaddr_i[16:13]][4] <= 0;end
+  						3'b100:begin LRU_S[vaddr_i[16:13]][0] <= 0;LRU_S[vaddr_i[16:13]][2] <= 1;LRU_S[vaddr_i[16:13]][5] <= 1;end
+  						3'b101:begin LRU_S[vaddr_i[16:13]][0] <= 0;LRU_S[vaddr_i[16:13]][2] <= 1;LRU_S[vaddr_i[16:13]][5] <= 0;end
+  						3'b110:begin LRU_S[vaddr_i[16:13]][0] <= 0;LRU_S[vaddr_i[16:13]][2] <= 0;LRU_S[vaddr_i[16:13]][6] <= 1;end
+  						3'b111:begin LRU_S[vaddr_i[16:13]][0] <= 0;LRU_S[vaddr_i[16:13]][2] <= 0;LRU_S[vaddr_i[16:13]][6] <= 0;end
+						default:;
+  					endcase
+				end
+			end else begin//不冲突时单独更新
+				if (hit_i) begin
+					case (i_hit_op)
+						1'b0:begin
+							case(i_hit_way[2:0])
+  								3'b000:begin LRU_S[vaddr_i[16:13]][0] <= 1;LRU_S[vaddr_i[16:13]][1] <= 1;LRU_S[vaddr_i[16:13]][3] <= 1;end
+  								3'b001:begin LRU_S[vaddr_i[16:13]][0] <= 1;LRU_S[vaddr_i[16:13]][1] <= 1;LRU_S[vaddr_i[16:13]][3] <= 0;end
+  								3'b010:begin LRU_S[vaddr_i[16:13]][0] <= 1;LRU_S[vaddr_i[16:13]][1] <= 0;LRU_S[vaddr_i[16:13]][4] <= 1;end
+  								3'b011:begin LRU_S[vaddr_i[16:13]][0] <= 1;LRU_S[vaddr_i[16:13]][1] <= 0;LRU_S[vaddr_i[16:13]][4] <= 0;end
+  								3'b100:begin LRU_S[vaddr_i[16:13]][0] <= 0;LRU_S[vaddr_i[16:13]][2] <= 1;LRU_S[vaddr_i[16:13]][5] <= 1;end
+  								3'b101:begin LRU_S[vaddr_i[16:13]][0] <= 0;LRU_S[vaddr_i[16:13]][2] <= 1;LRU_S[vaddr_i[16:13]][5] <= 0;end
+  								3'b110:begin LRU_S[vaddr_i[16:13]][0] <= 0;LRU_S[vaddr_i[16:13]][2] <= 0;LRU_S[vaddr_i[16:13]][6] <= 1;end
+  								3'b111:begin LRU_S[vaddr_i[16:13]][0] <= 0;LRU_S[vaddr_i[16:13]][2] <= 0;LRU_S[vaddr_i[16:13]][6] <= 0;end
+								default:;
+  							endcase
+						end
+						1'b1:begin
+							case(i_hit_way)
+								5'd0 :begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b1; LRU_M[3] <=1'b1; LRU_M[7] <=1'b1; LRU_M[15]<=1'b1;end
+								5'd1 :begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b1; LRU_M[3] <=1'b1; LRU_M[7] <=1'b1; LRU_M[15]<=1'b0;end
+								5'd2 :begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b1; LRU_M[3] <=1'b1; LRU_M[7] <=1'b0; LRU_M[16]<=1'b1;end
+								5'd3 :begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b1; LRU_M[3] <=1'b1; LRU_M[7] <=1'b0; LRU_M[16]<=1'b0;end
+								5'd4 :begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b1; LRU_M[3] <=1'b0; LRU_M[8] <=1'b1; LRU_M[17]<=1'b1;end
+								5'd5 :begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b1; LRU_M[3] <=1'b0; LRU_M[8] <=1'b1; LRU_M[17]<=1'b0;end
+								5'd6 :begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b1; LRU_M[3] <=1'b0; LRU_M[8] <=1'b0; LRU_M[18]<=1'b1;end
+								5'd7 :begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b1; LRU_M[3] <=1'b0; LRU_M[8] <=1'b0; LRU_M[18]<=1'b0;end
+								5'd8 :begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b0; LRU_M[4] <=1'b1; LRU_M[9] <=1'b1; LRU_M[19]<=1'b1;end
+								5'd9 :begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b0; LRU_M[4] <=1'b1; LRU_M[9] <=1'b1; LRU_M[19]<=1'b0;end
+								5'd10:begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b0; LRU_M[4] <=1'b1; LRU_M[9] <=1'b0; LRU_M[20]<=1'b1;end
+								5'd11:begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b0; LRU_M[4] <=1'b1; LRU_M[9] <=1'b0; LRU_M[20]<=1'b0;end
+								5'd12:begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b0; LRU_M[4] <=1'b0; LRU_M[10]<=1'b1; LRU_M[21]<=1'b1;end
+								5'd13:begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b0; LRU_M[4] <=1'b0; LRU_M[10]<=1'b1; LRU_M[21]<=1'b0;end
+								5'd14:begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b0; LRU_M[4] <=1'b0; LRU_M[10]<=1'b0; LRU_M[22]<=1'b1;end
+								5'd15:begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b0; LRU_M[4] <=1'b0; LRU_M[10]<=1'b0; LRU_M[22]<=1'b0;end
+								5'd16:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b1; LRU_M[5] <=1'b1; LRU_M[11]<=1'b1; LRU_M[23]<=1'b1;end
+								5'd17:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b1; LRU_M[5] <=1'b1; LRU_M[11]<=1'b1; LRU_M[23]<=1'b0;end
+								5'd18:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b1; LRU_M[5] <=1'b1; LRU_M[11]<=1'b0; LRU_M[24]<=1'b1;end
+								5'd19:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b1; LRU_M[5] <=1'b1; LRU_M[11]<=1'b0; LRU_M[24]<=1'b0;end
+								5'd20:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b1; LRU_M[5] <=1'b0; LRU_M[12]<=1'b1; LRU_M[25]<=1'b1;end
+								5'd21:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b1; LRU_M[5] <=1'b0; LRU_M[12]<=1'b1; LRU_M[25]<=1'b0;end
+								5'd22:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b1; LRU_M[5] <=1'b0; LRU_M[12]<=1'b0; LRU_M[26]<=1'b1;end
+								5'd23:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b1; LRU_M[5] <=1'b0; LRU_M[12]<=1'b0; LRU_M[26]<=1'b0;end
+								5'd24:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b0; LRU_M[6] <=1'b1; LRU_M[13]<=1'b1; LRU_M[27]<=1'b1;end
+								5'd25:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b0; LRU_M[6] <=1'b1; LRU_M[13]<=1'b1; LRU_M[27]<=1'b0;end
+								5'd26:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b0; LRU_M[6] <=1'b1; LRU_M[13]<=1'b0; LRU_M[28]<=1'b1;end
+								5'd27:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b0; LRU_M[6] <=1'b1; LRU_M[13]<=1'b0; LRU_M[28]<=1'b0;end
+								5'd28:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b0; LRU_M[6] <=1'b0; LRU_M[14]<=1'b1; LRU_M[29]<=1'b1;end
+								5'd29:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b0; LRU_M[6] <=1'b0; LRU_M[14]<=1'b1; LRU_M[29]<=1'b0;end
+								5'd30:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b0; LRU_M[6] <=1'b0; LRU_M[14]<=1'b0; LRU_M[30]<=1'b1;end
+								5'd31:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b0; LRU_M[6] <=1'b0; LRU_M[14]<=1'b0; LRU_M[30]<=1'b0;end
+								default:;
+  							endcase
+						end
+						default:;
+					endcase
+				end
+				if (hit_d) begin
+					case (d_hit_op)
+						1'b0:begin
+							case(d_hit_way[2:0])
+  								3'b000:begin LRU_S[dtlb_addr][0] <= 1;LRU_S[dtlb_addr][1] <= 1;LRU_S[dtlb_addr][3] <= 1;end
+  								3'b001:begin LRU_S[dtlb_addr][0] <= 1;LRU_S[dtlb_addr][1] <= 1;LRU_S[dtlb_addr][3] <= 0;end
+  								3'b010:begin LRU_S[dtlb_addr][0] <= 1;LRU_S[dtlb_addr][1] <= 0;LRU_S[dtlb_addr][4] <= 1;end
+  								3'b011:begin LRU_S[dtlb_addr][0] <= 1;LRU_S[dtlb_addr][1] <= 0;LRU_S[dtlb_addr][4] <= 0;end
+  								3'b100:begin LRU_S[dtlb_addr][0] <= 0;LRU_S[dtlb_addr][2] <= 1;LRU_S[dtlb_addr][5] <= 1;end
+  								3'b101:begin LRU_S[dtlb_addr][0] <= 0;LRU_S[dtlb_addr][2] <= 1;LRU_S[dtlb_addr][5] <= 0;end
+  								3'b110:begin LRU_S[dtlb_addr][0] <= 0;LRU_S[dtlb_addr][2] <= 0;LRU_S[dtlb_addr][6] <= 1;end
+  								3'b111:begin LRU_S[dtlb_addr][0] <= 0;LRU_S[dtlb_addr][2] <= 0;LRU_S[dtlb_addr][6] <= 0;end
+								default:;
+  							endcase
+						end
+						1'b1:begin
+							case(d_hit_way)
+								5'd0 :begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b1; LRU_M[3] <=1'b1; LRU_M[7] <=1'b1; LRU_M[15]<=1'b1;end
+								5'd1 :begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b1; LRU_M[3] <=1'b1; LRU_M[7] <=1'b1; LRU_M[15]<=1'b0;end
+								5'd2 :begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b1; LRU_M[3] <=1'b1; LRU_M[7] <=1'b0; LRU_M[16]<=1'b1;end
+								5'd3 :begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b1; LRU_M[3] <=1'b1; LRU_M[7] <=1'b0; LRU_M[16]<=1'b0;end
+								5'd4 :begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b1; LRU_M[3] <=1'b0; LRU_M[8] <=1'b1; LRU_M[17]<=1'b1;end
+								5'd5 :begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b1; LRU_M[3] <=1'b0; LRU_M[8] <=1'b1; LRU_M[17]<=1'b0;end
+								5'd6 :begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b1; LRU_M[3] <=1'b0; LRU_M[8] <=1'b0; LRU_M[18]<=1'b1;end
+								5'd7 :begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b1; LRU_M[3] <=1'b0; LRU_M[8] <=1'b0; LRU_M[18]<=1'b0;end
+								5'd8 :begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b0; LRU_M[4] <=1'b1; LRU_M[9] <=1'b1; LRU_M[19]<=1'b1;end
+								5'd9 :begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b0; LRU_M[4] <=1'b1; LRU_M[9] <=1'b1; LRU_M[19]<=1'b0;end
+								5'd10:begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b0; LRU_M[4] <=1'b1; LRU_M[9] <=1'b0; LRU_M[20]<=1'b1;end
+								5'd11:begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b0; LRU_M[4] <=1'b1; LRU_M[9] <=1'b0; LRU_M[20]<=1'b0;end
+								5'd12:begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b0; LRU_M[4] <=1'b0; LRU_M[10]<=1'b1; LRU_M[21]<=1'b1;end
+								5'd13:begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b0; LRU_M[4] <=1'b0; LRU_M[10]<=1'b1; LRU_M[21]<=1'b0;end
+								5'd14:begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b0; LRU_M[4] <=1'b0; LRU_M[10]<=1'b0; LRU_M[22]<=1'b1;end
+								5'd15:begin LRU_M[0]<=1'b1; LRU_M[1]<=1'b0; LRU_M[4] <=1'b0; LRU_M[10]<=1'b0; LRU_M[22]<=1'b0;end
+								5'd16:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b1; LRU_M[5] <=1'b1; LRU_M[11]<=1'b1; LRU_M[23]<=1'b1;end
+								5'd17:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b1; LRU_M[5] <=1'b1; LRU_M[11]<=1'b1; LRU_M[23]<=1'b0;end
+								5'd18:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b1; LRU_M[5] <=1'b1; LRU_M[11]<=1'b0; LRU_M[24]<=1'b1;end
+								5'd19:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b1; LRU_M[5] <=1'b1; LRU_M[11]<=1'b0; LRU_M[24]<=1'b0;end
+								5'd20:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b1; LRU_M[5] <=1'b0; LRU_M[12]<=1'b1; LRU_M[25]<=1'b1;end
+								5'd21:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b1; LRU_M[5] <=1'b0; LRU_M[12]<=1'b1; LRU_M[25]<=1'b0;end
+								5'd22:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b1; LRU_M[5] <=1'b0; LRU_M[12]<=1'b0; LRU_M[26]<=1'b1;end
+								5'd23:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b1; LRU_M[5] <=1'b0; LRU_M[12]<=1'b0; LRU_M[26]<=1'b0;end
+								5'd24:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b0; LRU_M[6] <=1'b1; LRU_M[13]<=1'b1; LRU_M[27]<=1'b1;end
+								5'd25:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b0; LRU_M[6] <=1'b1; LRU_M[13]<=1'b1; LRU_M[27]<=1'b0;end
+								5'd26:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b0; LRU_M[6] <=1'b1; LRU_M[13]<=1'b0; LRU_M[28]<=1'b1;end
+								5'd27:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b0; LRU_M[6] <=1'b1; LRU_M[13]<=1'b0; LRU_M[28]<=1'b0;end
+								5'd28:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b0; LRU_M[6] <=1'b0; LRU_M[14]<=1'b1; LRU_M[29]<=1'b1;end
+								5'd29:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b0; LRU_M[6] <=1'b0; LRU_M[14]<=1'b1; LRU_M[29]<=1'b0;end
+								5'd30:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b0; LRU_M[6] <=1'b0; LRU_M[14]<=1'b0; LRU_M[30]<=1'b1;end
+								5'd31:begin LRU_M[0]<=1'b0; LRU_M[2]<=1'b0; LRU_M[6] <=1'b0; LRU_M[14]<=1'b0; LRU_M[30]<=1'b0;end
+								default:;
+  							endcase
+						end
+						default:; 
+					endcase
+				end
+			end
+		end
+	end
 endmodule
